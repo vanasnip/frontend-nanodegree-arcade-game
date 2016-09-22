a3111e07980ce8f10f9bcf891bdcdf5d3df7e371
vanasnip <ivan.mamenemuno@gmail.com>
2016-09-22 22:49:24 +0100

    Added suggested changes from udacity code review NO1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+  "use strict";
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     this.x = x;
@@ -36,10 +38,10 @@ Enemy.prototype.respawn = function() {
 // a handleInput() method.
 //-------------------------------------------------------------------------
 // Enemies our player must avoid
+
 var Player = function(x, y) {
     // Variables applied to each of our instances go here,
-    this.x = x;
-    this.y = y;
+    Enemy.call(this, x, y);
     this.victory = false;
     this.allLivesLost = false;
     this.gemCollected = 0;
@@ -49,9 +51,9 @@ var Player = function(x, y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-boy.png';
-
 };
 
+Player.prototype = Object.create(Enemy.prototype); //inherit render functionality from Enemy
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
@@ -64,56 +66,51 @@ Player.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); // draw player
-};
-//-------------------------------------------------------------------------
+
 
 Player.prototype.handleInput = function(key){
 // if the player has'nt won and hasnt lost all their lives continue
-if (this.victory === false && this.allLivesLost === false){
-  if (key === "up" || key === "down"){
-    switch(key) {
-      case 'up':
-        if (this.y > -20){ // when UP is pressed before last row before boundery
-          this.y = this.y - 80; // move up to next row
-        }
-        if (this.y === -20){ //if last row is reached then the game is won
-          this.victory = true;
-        }
-        break;
-      case 'down':
-        if (this.y < 380){ // when DOWN is pressed
-          this.y = this.y + 80; //move to row bellow
-        }
-        break;
-      default:
-          return;
-    }
-  }
-
-  if (key === "left" || key === "right"){
-    switch(key) {
-      case 'left':
-          if (this.x > 0){ // left most boundery
-            this.x = this.x - 100;// move to column to the left
+  if (this.victory === false && this.allLivesLost === false){
+    if (key === "up" || key === "down"){
+      switch(key) {
+        case 'up':
+          if (this.y > -20){ // when UP is pressed before last row before boundery
+            this.y = this.y - 80; // move up to next row
+          }
+          if (this.y === -20){ //if last row is reached then the game is won
+            this.victory = true;
           }
           break;
-      case 'right':
-          if (this.x < 400){// right most boundery
-            this.x = this.x + 100; //more to column to the right
+        case 'down':
+          if (this.y < 380){ // when DOWN is pressed
+            this.y = this.y + 80; //move to row bellow
           }
           break;
-      default:
-          return;
+        default:
+            return;
+      }
+    }
+    if (key === "left" || key === "right"){
+      switch(key) {
+        case 'left':
+            if (this.x > 0){ // left most boundery
+              this.x = this.x - 100;// move to column to the left
+            }
+            break;
+        case 'right':
+            if (this.x < 400){// right most boundery
+              this.x = this.x + 100; //more to column to the right
+            }
+            break;
+        default:
+            return;
+      }
     }
   }
-}
 };
 
 Player.prototype.collision = function(){
-
-  for(var i = 0; i < allEnemies.length; i++){
+  for(var i = 0, len = allEnemies.length; i < len; i++){
     // conditions to check if any enemy is in the same space as the player
     if (allEnemies[i].x > this.x -80 && allEnemies[i].x < this.x + 80 && allEnemies[i].y === this.y){
       //resting the player position
@@ -128,15 +125,13 @@ Player.prototype.collision = function(){
     // conditions to check if any enemy is in the same space as the player
     if (allGems[j].x > this.x -80 && allGems[j].x < this.x + 80 && allGems[j].y === this.y){
       //resting the player position
-      allGems[j].x = 5 + (45 * player.gemCollected); // creates offset of 45 units to the right of last gem drawn
+      allGems[j].x = 5 + (45 * this.gemCollected); // creates offset of 45 units to the right of last gem drawn
       allGems[j].y = 520; // positions as bottom of canvas
       allGems[j].v = 35.438596491; //sets the width of gem drawn on canvas
       allGems[j].w = 60; // sets the height of gem drawn on canvas
-      player.gemCollected++;
-
+      this.gemCollected++;
     }
   }
-
 };
 
 Player.prototype.victorious = function(){
@@ -155,7 +150,6 @@ Player.prototype.reset = function(){
   this.x = 200;
   this.y = 380;
   //coordinates bring player back to starting position
-
 };
 
 var Gem = function(gemColor){
@@ -168,61 +162,51 @@ var Gem = function(gemColor){
   this.spawnGem(); // draws gem randomly on canvas over bug path way
 };
 
+
+
 Gem.prototype.spawnGem = function(){
 // randomly allocated a y coordinate with in the confines of the 3 lanes with in which the bug travel
   var yrando = Math.random(); //generate yrandom decimal < 1,  decimal > 0
   if (yrando > 0.666666666){ // deviding yrandom number ranger into 3 of 3 to define this.y (row)
-    console.log("top row ");
     this.y = 60;
   } if (yrando < 0.666666666 && yrando > 0.333333333){ // 2 of 3
-    console.log("middle row ");
     this.y = 140;
   } if (yrando < 0.333333333) {// 1 of 3
-    console.log("bottom row ");
     this.y = 220;
   }
 
 // randomly allocated a x coordinate with in the confines of the 5 with in the 5 collumns
   var xrando = Math.random();
   if (xrando > 0.8){ // deviding xrandom number ranger into 1 of 5 to define this.x (column)
-    console.log("x = 0");
     this.x = 0;
   } if (xrando < 0.8 && xrando > 0.6){ // 2 of 5  x columns
-    console.log("x = 100");
     this.x = 100;
   } if (xrando < 0.6 && xrando > 0.4){ // 3 of 5 x columns
-    console.log("x = 200");
     this.x = 200;
   } if (xrando < 0.4 && xrando > 0.2){ // 4 of 5 x columns
-    console.log("x = 300");
     this.x = 300;
   } if (xrando < 0.2) {// 5 of 5 x columns
     this.x = 400;
-    console.log("x = 400");
   }
-  console.log(xrando);
-
 };
 
 Gem.prototype.render = function(gemCol) {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.v, this.w);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.v, this.w);
 };
 
 var Heart = function(x, y){
-  this.x = x;
-  this.y = y;
+  Enemy.call(this, x, y);
   this.v = 35.438596491; //sets the width of heart drawn on canvas
   this.w = 60; //sets the height of heart drawn on canvas
   this.sprite = 'images/Heart.png';
 };
 
-Heart.prototype.render = function(gemCol) {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.v, this.w);
-};
+Heart.prototype = Object.create(Gem.prototype); //inherit render functionality from Gem
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-//
 var Winner = function(){
   this.sprite = 'images/good job.png';
   this.x = 10;
@@ -270,18 +254,14 @@ var allHearts = [ // 3 hearts representing lives drawn in a row
 var winner = new Winner(); // initiated when player wins
 var gameOver = new GameOver(); // initiated when player has no more lives
 
-
-
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-    player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
